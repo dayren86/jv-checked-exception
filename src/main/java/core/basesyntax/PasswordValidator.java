8b6c80328f303f9e05f61075d38a8d5c17c58957
unknown <dayren86@gmail.com>
2025-10-22 20:03:38 +0300

    pswd valid v3

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -3,9 +3,13 @@ package core.basesyntax;
 public class PasswordValidator {
     public void validate(String password, String repeatPassword)
             throws PasswordValidationException {
-
-        if (!password.equals(repeatPassword) && password.length() > 10) {
+        if (password == null || repeatPassword == null) {
             throw new PasswordValidationException("Wrong Password");
+        }
+
+        if (!password.equals(repeatPassword) || password.length() < 10 ||
+                password.isBlank() || repeatPassword.isBlank()) {
+            throw new PasswordValidationException("Wrong passwords");
         }
     }
 }
